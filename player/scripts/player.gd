bd8ac2a56c702f0f14e4fd3ad192d9a9bd3ab5f5
Pedro Pinheiro <pepeaugust.10@gmail.com>
2025-02-07 00:52:27 -0300

    Dash animado corretamente

--- player/scripts/player.gd
+++ player/scripts/player.gd
@@ -3,15 +3,16 @@ class_name Player extends CharacterBody2D
 var cardinal : Vector2 = Vector2.DOWN
 var direction : Vector2 = Vector2.ZERO
 const speed : float = 500
-const dash_speed : float = 1700
+const dash_speed : float = 1400
 
 
 var state : String = "idle"
 var dash : bool = false
 var pode_dash: bool = true
 
-@onready var sprite: Sprite2D = $Sprite2D
+@onready var sprite: Sprite2D = $SpriteJogador
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite_dash: Sprite2D = $SpriteJogador/SpriteDash
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,50 +24,48 @@ func _process(delta: float) -> void:
 	
 	direction.y = Input.get_action_strength("baixo") - Input.get_action_strength("cima")
 	direction.x = Input.get_action_strength("direita") - Input.get_action_strength("esquerda")
-	
+	DirecaoPersonagem()
 	if Input.get_action_strength("dash") and pode_dash:
 		dash = true
 		$Timer_dash_duracao.start()
 		$Timer_dash_cooldown.start()
 	
+	DirecaoDash()
 	if dash:
+		sprite_dash.visible = true
 		velocity = direction.normalized() * dash_speed
 		pode_dash = false
 	else:
+		sprite_dash.visible = false
 		velocity = direction.normalized() * speed
-		
-	if DefineEstado() || DefineDirecao(): 
+	if DefineEstado():
 		AtualizaAnimacao()
 	pass
 
 func _physics_process(delta):
 	move_and_slide()
 
-func DefineDirecao() -> bool:
-	var nova_dir : Vector2 = cardinal
-	if direction == Vector2.ZERO:
-		return false
-	if direction.x == 0:
-		nova_dir = Vector2.UP if direction.y < 0 else Vector2.DOWN
-	if direction.y == 0:
-		nova_dir = Vector2.LEFT if direction.x < 0  else Vector2.RIGHT
-	if nova_dir == cardinal:
-		return false
-	cardinal = nova_dir
-	sprite.scale.x = -1 if cardinal == Vector2.LEFT else 1
-	return true
-
 func DefineEstado() -> bool:
-	var novo_estado : String = "idle" if direction == Vector2.ZERO else "andar"
+	var novo_estado : String = "idle" if direction == Vector2.ZERO else "dash"
 	if novo_estado == state:
 		return false
 	state = novo_estado
 	return true
 
 func AtualizaAnimacao() -> void:
-	#print(DirecaoAnimacao())
 	animation_player.play(state)
 	pass
+	
+func DirecaoPersonagem() -> void:
+	if direction.x < 0:
+		sprite.scale.x = -1
+	elif direction.x > 0:
+		sprite.scale.x = 1
+		
+func DirecaoDash() -> void:
+	var ang = atan2(direction.y,direction.x)
+	if abs(ang) > 1.571: ang = -ang + 3.14 
+	sprite_dash.rotation = ang
 
 func _on_timer_dash_duracao_timeout() -> void:
 	dash = false
